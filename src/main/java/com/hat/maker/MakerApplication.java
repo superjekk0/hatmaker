6c9bf860c89b9131c0c0ba173b78592cbcbe601f
Emanuel Paige <2143602@claurendeau.qc.ca>
2025-02-05 14:18:37 -0500

    SCRUM-13: Bootstrappé le responsable au lancement de l'applicaiton

--- src/main/java/com/hat/maker/MakerApplication.java
+++ src/main/java/com/hat/maker/MakerApplication.java
@@ -1,13 +1,28 @@
 package com.hat.maker;
 
+import com.hat.maker.service.ResponsableService;
+import com.hat.maker.service.dto.ResponsableCreeDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MakerApplication {
+@RequiredArgsConstructor
+public class MakerApplication implements CommandLineRunner {
+	private final ResponsableService responsableService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MakerApplication.class, args);
 	}
 
+	public void run(String... args) {
+		ResponsableCreeDTO responsableDTO = ResponsableCreeDTO.builder()
+				.id(1L)
+				.nom("Le Impact")
+				.courriel("leimpact@cc.com")
+				.motDePasse("1")
+				.build();
+		responsableService.createResponsable(responsableDTO);
+	}
 }
