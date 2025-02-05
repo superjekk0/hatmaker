6d61cd326a784350f457d41d7811556e71855c5e
Emanuel Paige <2143602@claurendeau.qc.ca>
2025-02-05 15:49:00 -0500

    SCRUM-13: Changement des messages d'erreurs

--- src/main/java/com/hat/maker/security/exception/UserNotFoundException.java
+++ src/main/java/com/hat/maker/security/exception/UserNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class UserNotFoundException extends APIException {
         public UserNotFoundException() {
-            super(HttpStatus.NOT_FOUND,"userNotFound");
+            super(HttpStatus.NOT_FOUND,"Utilisateur non trouvé");
 
         }
 }
