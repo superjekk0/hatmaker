c18a94a737f4d85e25bcb33317562e2e3e455759
superjekk <superjekk@outlook.com>
2025-06-08 15:32:30 -0400

    BUGFIX : Mappage autorisé

--- src/main/java/com/hat/maker/MakerApplication.java
+++ src/main/java/com/hat/maker/MakerApplication.java
@@ -36,7 +36,7 @@ public class MakerApplication implements CommandLineRunner {
          return new WebMvcConfigurer() {
              @Override
              public void addCorsMappings(CorsRegistry registry) {
-                 registry.addMapping("*")
+                 registry.addMapping("/**")
                          .allowedOrigins(crossOrigin)
                          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                          .allowedHeaders("*")
